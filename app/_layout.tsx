375a883436482a1290634369e34a2b7653a3287a
eisavand_fa <esavand.fa@gmail.com>
2025-04-30 18:45:37 +0330

    first

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,6 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
@@ -31,6 +30,7 @@ export default function RootLayout() {
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="biography" options={{ title: 'Biography' }} />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
